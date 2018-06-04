3ed7221d0899730d1245350769c4ace9ea61472a
james.asj <james@jamesalves.com.br>
2018-06-04 12:57:37 -0300

    ajustado leitura da linha do notas treino

--- Principal.py
+++ Principal.py
@@ -6,11 +6,11 @@ def nome(arquivo):
     config = ConfigController(arquivo)
     prolog = PrologController()
     notas_treino = NotasTreinoController(config.obterDiretorio)
-    lAlunos = notas_treino.obterListaAlunos()
     
-    for(aluno in lAlunos):
+    while(notas_treino.proximo()):
+        aluno = notas_treino.aluno()
         arquivo = aluno[3]
         resultado = prolog.executar(arquivo)
         aluno[4] = resultado
-        
+
     notas_treino.atualizar(lAlunos)
